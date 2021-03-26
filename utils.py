ed4b1fa09d249374533ba296a873d00e947eee05
Anton144 <anton-paramonov2000@yandex.ru>
2021-03-26 23:41:08 +0300

    lattice

--- utils.py
+++ utils.py
@@ -79,7 +79,36 @@ def X_gate():
     return x_gate
 
 
-def linear_circuit(n, interactions, magnetic_fields=None, beta=1):
+def Omega_gate(J=1, beta=1):
+	from qiskit.quantum_info.operators import Operator
+	from qiskit.extensions import UnitaryGate
+
+	x = np.exp(beta/2)
+	c = 0.5 * Z(2, {(0, 1): J}, beta=beta)
+	S_mat = 1/c**(1/2) * np.array(
+	[
+		[x**J, x**(-J), 0, 0],
+		[-x**(-J), x**J, 0, 0],
+		[0, 0, x**(-J), x**J],
+		[0, 0, -x**J, x**(-J)]
+	]
+	)
+	X_mat = np.array([
+        [0, -1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, -1, 0],
+        [0, 0, 0, 1]
+    ]
+	)
+    
+	S_operator, X_operator = Operator(S_mat), Operator(X_mat)
+
+	res_operator = Operator(np.eye(8)).compose(S_operator, qargs=[2, 0]).compose(X_operator, qargs=[2, 1])
+
+	return UnitaryGate(res_operator, 'Omega')
+
+
+def linear_circuit(n, interactions, beta=1):
 	qr = QuantumRegister(n, 'q')
 	circ = QuantumCircuit(qr)
 	circ.h(0)
@@ -88,14 +117,57 @@ def linear_circuit(n, interactions, magnetic_fields=None, beta=1):
 	return circ
 
 
-def circled_circuit(n, interactions, magnetic_fields=None, beta=1):
-	circ = linear_circuit(n, interactions, magnetic_fields, beta)
+def circled_circuit(n, interactions, beta=1):
+	circ = linear_circuit(n, interactions, beta)
 	w = QuantumRegister(1, 'w')
 	circ.add_register(w)
-	circ.append(S_gate(interactions[n - 1, 0], beta), [w[0], circ.qubits[-2]])
-	circ.append(X_gate(), [w[0], circ.qubits[0]])
+	circ.append(Omega_gate(interactions[n - 1, 0], beta), [circ.qubits[-2], circ.qubits[0], w[0]])
 
 	return circ
+
+
+def lattice_draft(plaquettes_interactions, hv_edges_n, beta=1):
+	VERTICES_IN_PLAQUETTE = 4
+
+	plaquettes_n = len(plaquettes_interactions)
+	qr = QuantumRegister(plaquettes_n * VERTICES_IN_PLAQUETTE, 'q')
+	w_close = QuantumRegister(plaquettes_n, 'wClose')
+	circ = QuantumCircuit(qr, w_close)
+	for i, interactions in enumerate(plaquettes_interactions):
+		pairs = list(interactions.keys())
+		begin_qubit = pairs[0][0]
+		end_qubit = None
+		circ.h(qr[begin_qubit])
+		for frm, to in pairs:
+			if to == begin_qubit:
+				end_qubit = frm
+				continue
+			circ.append(S_gate(interactions[frm, to], beta), [qr[to], qr[frm]])
+
+		circ.append(Omega_gate(interactions[end_qubit, begin_qubit], beta), [qr[end_qubit], qr[begin_qubit], w_close[i]])
+
+	return circ
+
+
+def lattice_curcuit(plaquettes_interactions, hv_interactions, beta=1):
+	VERTICES_IN_PLAQUETTE = 4
+
+	hv_edges_n = len(hv_interactions.keys())
+	circ = lattice_draft(plaquettes_interactions, hv_edges_n, beta)
+	qr = circ.qregs[0]
+
+	w_connect = QuantumRegister(hv_edges_n, 'wConnect')
+	circ.add_register(w_connect)
+
+	for i, ((frm, to), interaction) in enumerate(hv_interactions.items()):
+		circ.append(Omega_gate(interaction, beta), [qr[frm], qr[to], w_connect[i]])
+
+	return circ
+
+
+def process_lattice_result(result, work_bits_n):
+	return  {key[work_bits_n:]: value for (key, value) in result.items() if key[:work_bits_n] == '1' * work_bits_n}
+
 
 
 def process_circled_result(result):
